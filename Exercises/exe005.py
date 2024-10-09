180a1931e371d600741aecf1e186769115742b4d
Dâmaris Campos <camposdamaris08@gmail.com>
2024-10-08 23:05:54 -0400

    News exercises

---
+++ Exercises/exe005.py
@@ -0,0 +1,4 @@
+num = int(input('Digite o número: '))
+
+print('O sucessor de {} é {}.'.format(num, num+1))
+print('O antecessor de {} é {}.'.format(num, num-1))
---
+++ Exercises/exe006.py
@@ -0,0 +1,3 @@
+num = int(input('Digite o número: '))
+
+print('O dobro de {} é {}, o triplo de {} é {} e a raiz quadrada de {} é {}.'.format(num, num*2, num, num*3, num, num**0.5))
