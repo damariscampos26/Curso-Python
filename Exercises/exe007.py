d894621bda872d99b0fbf20a3b63e72277c2ebff
Dâmaris Campos <camposdamaris08@gmail.com>
2024-10-10 14:20:20 -0400

    Modifications

--- Exercises/exe007.py
+++ Exercises/exe007.py
@@ -1,4 +1,4 @@
-nota1 = float(input('Digite a primeira nota: '))
-nota2 = float(input('Digite a segunda nota: '))
+nota1 = float(input('Digite a primeira nota do aluno: '))
+nota2 = float(input('Digite a segunda nota do aluno: '))
 media = (nota1 + nota2) / 2
 print(f'A média é {media:.1f}')
---
+++ Exercises/exe014.py
@@ -0,0 +1,3 @@
+celsius = float(input('Digite a temperatura em °C: '))
+
+print(f'A temperatura de {celsius}°C corresponde a {(celsius*1.8)+32:.1f}°F.')
