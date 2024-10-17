9f009b20241dc211044f7d4f1366e08e3a9fabed
Dâmaris Campos <camposdamaris08@gmail.com>
2024-10-17 15:58:48 -0400

    Updated Exercises

--- Exercises/exe021.py
+++ Exercises/exe021.py
@@ -1,3 +1,8 @@
 # Faça um programa que abra e reproduza o áudio de um arquivo MP3.
 import pygame
+
 pygame.init()
+pygame.mixer.music.load('/home/damaris/Área de trabalho/Curso Python/Exercises/exe021.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
