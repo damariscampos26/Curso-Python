86a65adc9d217260b79e8574b0a54241361cacad
Dâmaris Campos <125172769+damariscampos26@users.noreply.github.com>
2024-11-04 22:25:44 -0400

    Update exe046.py

--- Mundo 2/exe046.py
+++ Mundo 2/exe046.py
@@ -1,4 +1,4 @@
-# Faça um programa que mostre na tela uma contaagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa
+# Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa
 # de 1s entre eles.
 from time import sleep
 
