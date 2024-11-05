4f03d897dde29e95947135c391fc14451997bc0c
Dâmaris Campos <125172769+damariscampos26@users.noreply.github.com>
2024-11-04 22:26:35 -0400

    Update exe048.py

--- Mundo 2/exe048.py
+++ Mundo 2/exe048.py
@@ -1,4 +1,4 @@
-# Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de três e que se encontram no intervalo de d
+# Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de três e que se encontram no intervalo de
 # 1 até 500.
 j = 0
 for i in range(1, 501):
