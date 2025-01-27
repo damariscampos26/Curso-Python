48ab44781d83e2295e7b0c14c0aa2cdde254a8ca
Dâmaris Campos <camposdamaris08@gmail.com>
2025-01-27 16:33:23 -0400

    Updated Exercise

--- Mundo 2/exe065.py
+++ Mundo 2/exe065.py
@@ -2,17 +2,27 @@
 # qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 from time import sleep
 media = 0
-contador = 0
+quantidade = 0
 soma = 0
-opcao = int(input('\nDigite um número (0 para sair): '))
+maior = 0
+menor = 0
 
-while opcao != 0:
-    soma += opcao
-    contador +=1
-    opcao = int(input('\nDigite um número (0 para sair): '))
-    media = soma / contador
+numero = int(input('\nDigite um número (0 para sair): '))
+while numero != 0:
+    soma += numero
+    quantidade +=1
+    if quantidade == 1:
+        maior = menor = numero
+    else:
+        if numero > maior:
+            maior = numero
+        elif numero < menor:
+            menor = numero
+    numero = int(input('\nDigite um número (0 para sair): '))
+
+media = soma / quantidade
 
 sleep(1)
 print('\nVocê saiu do programa!')
 sleep(1)
-print(f'\nA média dos números digitados é {media}.')
+print(f'\nA média dos números digitados é {media:.2f}. \nO maior número digitado foi {maior}. \nO menor número digitado foi {menor}')
